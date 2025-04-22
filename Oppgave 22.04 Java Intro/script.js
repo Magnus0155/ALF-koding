cd42f1725d32bd3beb818e2d8b67f23f49f7944a
Magnus <magnuseeg1@gmail.com>
2025-04-22 13:56:17 +0200

    123

---
+++ Oppgave 22.04 Java Intro/script.js
@@ -0,0 +1,32 @@
+/*Oppgave1*/
+let a = 5
+let b = 7.5
+let c = 10
+const d = 12.5
+const e = 15
+
+ console.log(a*b)
+/* Oppgave2 
+ window.onload = function () {
+ let navn = prompt("SKriv inn ditt navn")
+ let favorittFarge = prompt("Skriv inn din favorittfarge")
+ let svar = "hei" + navn+ "er din favorittfarge" + favorittFarge+"?";
+
+ document.getElementById("spørsmål").innerText = svar
+}
+*/
+
+/*Opppgave3*/
+function navn() {
+let forNavn = document.getElementById("fornavn").value
+let mellomNavn = document.getElementById("mellomnavn").value
+let etterNavn = document.getElementById("etternavn").value
+
+let fulltNavn = forNavn + " " + mellomNavn + " " + etterNavn
+
+document.getElementById("fulltnavn").textContent = fulltNavn
+
+}
+
+/*Oppgave4*/
+

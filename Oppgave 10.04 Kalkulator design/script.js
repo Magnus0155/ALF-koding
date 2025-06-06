c5ad2c866ef4253c5264103a34e285f4c0d65753
Magnus <magnuseeg1@gmail.com>
2025-06-06 10:35:28 +0200

    Kalk

--- Oppgave 10.04 Kalkulator design/script.js
+++ Oppgave 10.04 Kalkulator design/script.js
@@ -1,13 +1,20 @@
-let equalEl = document.getElementById("display")
+function addToDisplay(value) {
+    document.getElementById('display').value += value
+}
 
-
+function clearDisplay() {
+    document.getElementById('display').value = ''
+}
 
 
 
 
 function special() {
-
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value)
+    } catch (error) {
+        document.getElementById('display').value = 'Error'
+    }
     console.log("Button is clicked")
 }
 
-special()
